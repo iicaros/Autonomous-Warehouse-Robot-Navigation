ebc3a5ad941203b991272f1c242e8be758405085
iikahros <icarussbliss@gmail.com>
2025-04-23 21:13:20 -0700

    first test case/ finds start and end

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ warehouse_map = [
     ['.', '.', '.', 'X', '.']
 ]
 
-# Movement directions, up, down , left, right
+# Movement directions: up, down, left, right
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 cost_map = {
-    ',': 1,
+    '.': 1,
     'P': 0.5,
     'S': 0,
     'D': 1
@@ -20,14 +21,18 @@ cost_map = {
 
 def start_end(grid):
     """
-    Finds the positions of the start and end of the path.  
+    Finds the positions of the start and end of the path.
     """
     start = end = None
-    for r in range (len(grid)):
-        for c in range (len(grid[0])):
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
             if grid[r][c] == "S":
                 start = (r, c)
             elif grid[r][c] == "D":
                 end = (r, c)
-
     return start, end
+
+
+start, end = start_end(warehouse_map)
+print("Start:", start)
+print("End:", end)
